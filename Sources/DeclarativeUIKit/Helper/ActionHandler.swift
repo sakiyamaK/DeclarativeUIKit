2583fa43dc62bf0a8479a219b0c896f2cba6c8c1
sakiyamaK <sakiyama.k@gmail.com>
2022-07-04 23:34:18 +0900

    uibuttonのactionとgesturerecognizerのメソッドをリアクティブに対応

---
+++ Sources/DeclarativeUIKit/Helper/ActionHandler.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by sakiyamaK on 2022/07/04.
+//
+
+import Foundation
+
+final class ActionHandler {
+    weak var sender: AnyObject!
+    private let action: (Any) -> Void
+    
+    init(target: Any, action: @escaping (Any) -> Void) {
+        self.action = action
+        objc_setAssociatedObject(target, String(arc4random()), self, .OBJC_ASSOCIATION_RETAIN)
+    }
+    
+    init(sender: AnyObject, target: Any, action: @escaping (Any) -> Void) {
+        self.sender = sender
+        self.action = action
+        objc_setAssociatedObject(target, String(arc4random()), self, .OBJC_ASSOCIATION_RETAIN)
+    }
+
+    @objc func invoke() { action(sender as Any) }
+
+    static var selector: Selector { #selector(ActionHandler.invoke) }
+}
--- Sources/DeclarativeUIKit/ViewExtension/UIGestureRecognizer+.swift
+++ Sources/DeclarativeUIKit/ViewExtension/UIGestureRecognizer+.swift
@@ -4,5 +4,10 @@ public extension UIGestureRecognizer {
     convenience init(target: Any?, _ actionBuilder: () -> Selector?) {
         self.init(target: target, action: actionBuilder())
     }
+    
+    convenience init(target: Any, _ action: @escaping ((Any) -> Void)) {
+        let handler = ActionHandler(target: target, action: action)
+        self.init(target: handler, action: ActionHandler.selector)
+        handler.sender = self
+    }
 }
-
