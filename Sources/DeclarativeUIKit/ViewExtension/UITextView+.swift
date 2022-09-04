7fa7e3abe5a9b4c520116cd4e5865759d46445ab
Hajime Taniguchi <hameji@gmail.com>
2022-09-03 20:12:04 -0400

    textを用意

--- Sources/DeclarativeUIKit/ViewExtension/UITextView+.swift
+++ Sources/DeclarativeUIKit/ViewExtension/UITextView+.swift
@@ -5,4 +5,60 @@
 //  Created by Hajime Taniguchi on 9/3/22.
 //
 
-import Foundation
+import UIKit.UITextView
+import UIKit
+
+private extension UITextView {
+    @discardableResult
+    func setup(attrText: NSAttributedString?) -> Self {
+        guard let attrText = attrText else {
+            self.text = nil
+            self.attributedText = nil
+            return self
+        }
+        self.attributedText = attrText
+        return self
+    }
+}
+
+//MARK: -
+public extension UITextView {
+    convenience init(_ text: String?) {
+        self.init(frame: .zero)
+        self.text(text)
+    }
+        
+    convenience init(_ text: String?, _ imperative: (Self) -> Void) {
+        self.init(frame: .zero)
+        self.text(text)
+        imperative(self)
+    }
+
+    convenience init(_ text: NSAttributedString?) {
+        self.init(frame: .zero)
+        self.text(text)
+    }
+    
+    convenience init(_ text: NSAttributedString?, _ imperative: (Self) -> Void) {
+        self.init(frame: .zero)
+        self.text(text)
+        imperative(self)
+    }
+}
+
+//MARK: - Declarative method
+public extension UITextView {
+
+    @discardableResult
+    func text(_ text: String?) -> Self {
+        guard let text = text else {
+            return self.setup(attrText: nil)
+        }
+        return self.setup(attrText: NSAttributedString(string: text))
+    }
+    
+    @discardableResult
+    func text(_ attrText: NSAttributedString?) -> Self {
+        return self.setup(attrText: attrText)
+    }
+}
