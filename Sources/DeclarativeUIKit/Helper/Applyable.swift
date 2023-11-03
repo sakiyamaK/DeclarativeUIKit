f5925c62f3f25ae6df41e6eb0d34b7b22e198cb2
sakiyamaK <sakiyama.k@gmail.com>
2023-11-03 15:04:40 +0900

    update applyable

--- Sources/DeclarativeUIKit/Helper/Applyable.swift
+++ Sources/DeclarativeUIKit/Helper/Applyable.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 // MARK: - Applyable
-public protocol Applyable: AnyObject {}
+public protocol Applyable {}
 extension Applyable {
     @discardableResult
     public func apply(_ closure: (Self) -> Void) -> Self {
@@ -22,3 +22,8 @@ extension UIBezierPath: Applyable {}
 extension UIViewController: Applyable {}
 @available(iOS 13.0, *)
 extension UIBarAppearance: Applyable {}
+extension String: Applyable {}
+extension NSString: Applyable {}
+@available(iOS 15.0, *)
+extension AttributedString: Applyable {}
+extension NSAttributedString: Applyable {}
